ab6de4b071dc6da0aa9009aaf7519f592bcfb317
Sendoa6 <sendoa.perez06@somo.eus>
2025-12-02 13:39:51 +0100

    esqueleto

---
+++ BD/conexiones.php
@@ -0,0 +1,10 @@
+<?php
+$conexion = new mysqli("localhost", "root", "", "salsagram_db");
+//$conexion = new mysqli("localhost", "root", "", "videojuegos_db");
+
+// if ($conexion->connect_error) {
+//     die("❌ Error de conexión: " . $conexion->connect_error);
+// } else {
+//     echo "✅ Conexión exitosa";
+// }
+?>
