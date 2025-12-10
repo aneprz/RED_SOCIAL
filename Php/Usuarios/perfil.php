81f9875d7777e88d107b293d67af162b17a59af0
Miikeel16 <mikel.garcia06@somo.eus>
2025-12-10 11:10:25 +0100

    cambios

--- Php/Usuarios/perfil.php
+++ Php/Usuarios/perfil.php
@@ -1,15 +1,17 @@
-
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Perfil</title>
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="../../Estilos/estilos_perfil.css">
 </head>
 <body>
-    <?php include __DIR__ . '/../Templates/navBar.php';?>
-    <?php include __DIR__ . '/../Templates/footer.php';?>
+    <?php
+      $nombreusu= $_SESSION['username'];
+      echo $nombreusu;
+    ?>
 </body>
 </html>
 
