c9f16db37d45347903e2dc0f941ad711163eaf1d
aneprz <aneperezsenin1@gmail.com>
2025-12-10 08:45:41 +0100

    Sesiones arreglos

--- Php/Sesiones/procesamientos/procesar_inicio_sesion.php
+++ Php/Sesiones/procesamientos/procesar_inicio_sesion.php
@@ -6,30 +6,36 @@ include '../../../BD/conexiones.php';
 // Recibir los datos del formulario y limpiarlos
 $nombre_usuario= $_POST['nombre_usuario'];
 $contraseña= $_POST['contraseña'];
-$contraseña=hash('sha256', $contraseña);
 
-
-$query = "SELECT * FROM usuarios WHERE username = '$nombre_usuario' AND password_hash = '$contraseña' LIMIT 1";
-
+//Busca al usuario por el nombre
+$query = "SELECT * FROM usuarios WHERE username = '$nombre_usuario' LIMIT 1";
 $result = mysqli_query($conexion, $query);
 
 // Verificar si el usuario existe
 if ($result && mysqli_num_rows($result) > 0) {
     $datos_usuario = mysqli_fetch_assoc($result);
     
-    // Guardar datos del usuario en la sesión
-    $_SESSION['id'] = $datos_usuario['id'];
-    $_SESSION['username'] = $datos_usuario['username'];
-    $_SESSION['admin'] = $datos_usuario['admin'];
-    $_SESSION['email'] = $datos_usuario['email'];
+    if (password_verify($contraseña, $datos_usuario['password_hash'])) {
+        // Guardar datos del usuario en la sesión
+        $_SESSION['id'] = $datos_usuario['id'];
+        $_SESSION['username'] = $datos_usuario['username'];
+        $_SESSION['admin'] = $datos_usuario['admin'];
 
-    // Redirigir a la página de bienvenida
-    header("location: ../../index.php");
-    exit();
+        // Redirigir a la página de bienvenida
+        header("location: ../../../index.php");
+        exit(); 
+    } else {
+        // Contraseña incorrecta
+        echo "<script>
+                alert('Contraseña incorrecta.');
+                window.location.href = '../inicio_sesion.php';
+              </script>";
+        exit();
+    }
 } else {
     // Si el usuario no existe o la contraseña es incorrecta
     echo "<script type='text/javascript'>
-        alert('Usuario o contraseña incorrectos. Intenta de nuevo.');
+        alert('El usuario no existe.');
         window.location.href = '../inicio_sesion.php';
     </script>";
     exit();
