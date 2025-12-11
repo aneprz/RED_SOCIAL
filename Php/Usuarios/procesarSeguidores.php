c15b3055edb2912d1873ec73e67a545765889629
Miikeel16 <mikel.garcia06@somo.eus>
2025-12-11 11:38:15 +0100

    a

--- Php/Usuarios/procesarSeguidores.php
+++ Php/Usuarios/procesarSeguidores.php
@@ -6,11 +6,6 @@ if (!isset($_SESSION['username'])) {
 }
 include '../../BD/conexiones.php';
 
-if (!isset($_SESSION['id'])) {
-    echo "Error: no has iniciado sesión.";
-    exit();
-}
-
 if (isset($_POST['id_usuario']) && isset($_POST['accion'])) {
     $id_usuario = intval($_POST['id_usuario']);
     $accion = $_POST['accion'];
@@ -19,7 +14,6 @@ if (isset($_POST['id_usuario']) && isset($_POST['accion'])) {
     if ($accion === 'suprimir') {
         $query = "DELETE FROM seguidores WHERE seguidor_id = $seguidor_id AND seguido_id = $id_usuario";
         if (!mysqli_query($conexion, $query)) {
-            echo "Error al eliminar: " . mysqli_error($conexion);
             exit();
         }
         header("Location: tablaSeguidos.php");
@@ -29,14 +23,12 @@ if (isset($_POST['id_usuario']) && isset($_POST['accion'])) {
         $check = "SELECT * FROM seguidores WHERE seguidor_id = $seguidor_id AND seguido_id = $id_usuario";
         $res = mysqli_query($conexion, $check);
         if (!$res) {
-            echo "Error en la consulta: " . mysqli_error($conexion);
             exit();
         }
 
         if (mysqli_num_rows($res) == 0) {
             $query = "INSERT INTO seguidores (seguidor_id, seguido_id) VALUES ($seguidor_id, $id_usuario)";
             if (!mysqli_query($conexion, $query)) {
-                echo "Error al seguir: " . mysqli_error($conexion);
                 exit();
             }
         }
@@ -45,7 +37,6 @@ if (isset($_POST['id_usuario']) && isset($_POST['accion'])) {
         exit();
     } 
 } else {
-    echo "Datos incompletos.";
     exit();
 }
 ?>
