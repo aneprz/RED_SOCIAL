3b00d555d5d1ea01fb822691a04693ad00bcf44d
Miikeel16 <mikel.garcia06@somo.eus>
2025-12-11 08:30:53 +0100

    cambios

--- Php/Usuarios/procesar_perfil.php
+++ Php/Usuarios/procesar_perfil.php
@@ -8,13 +8,17 @@ $id = intval($_SESSION['id']);
 
 $resultSeguidores = mysqli_query($conexion, "SELECT COUNT(seguidor_id) AS total FROM seguidores WHERE seguido_id = $id");
 
-$row = mysqli_fetch_assoc($resultSeguidores);
-$seguidores = $row['total'];
+$rowSeguidores = mysqli_fetch_assoc($resultSeguidores);
+$seguidores = $rowSeguidores['total'];
 
 //A CUANTOS SIGO
-
 $resultSeguidos = mysqli_query($conexion, "SELECT COUNT(seguido_id) AS total FROM seguidores WHERE seguido_id = $id");
 
-$row = mysqli_fetch_assoc($resultSeguidos);
-$seguidos = $row['total'];
+$rowSeguidos = mysqli_fetch_assoc($resultSeguidos);
+$seguidos = $rowSeguidos['total'];
+
+echo $id
+
+
 ?>
+
