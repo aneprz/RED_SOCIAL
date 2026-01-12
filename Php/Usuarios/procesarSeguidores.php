e2039bfb8c85ca5f542a2095d6f0e5e9373a4390
Miikeel16 <mikel.garcia06@somo.eus>
2026-01-12 11:39:35 +0100

    ay dios mio

--- Php/Usuarios/procesarSeguidores.php
+++ Php/Usuarios/procesarSeguidores.php
@@ -6,36 +6,26 @@ if (!isset($_SESSION['username'])) {
 }
 include '../../BD/conexiones.php';
 
-if (isset($_POST['id_usuario']) && isset($_POST['accion'])) {
+if (isset($_POST['id_usuario'], $_POST['accion'], $_POST['pagina_origen'])) {
     $id_usuario = intval($_POST['id_usuario']);
     $accion = $_POST['accion'];
-    $seguidor_id = $_SESSION['id'];
+    $seguidor_id = intval($_SESSION['id']);
+    $pagina_origen = $_POST['pagina_origen'];
 
     if ($accion === 'suprimir') {
         $query = "DELETE FROM seguidores WHERE seguidor_id = $seguidor_id AND seguido_id = $id_usuario";
-        if (!mysqli_query($conexion, $query)) {
-            exit();
-        }
-        header("Location: tablaSeguidos.php");
-        exit();
-
+        mysqli_query($conexion, $query);
     } elseif ($accion === 'seguir') {
-        $check = "SELECT * FROM seguidores WHERE seguidor_id = $seguidor_id AND seguido_id = $id_usuario";
+        $check = "SELECT 1 FROM seguidores WHERE seguidor_id = $seguidor_id AND seguido_id = $id_usuario";
         $res = mysqli_query($conexion, $check);
-        if (!$res) {
-            exit();
-        }
-
-        if (mysqli_num_rows($res) == 0) {
+        if ($res && mysqli_num_rows($res) === 0) {
             $query = "INSERT INTO seguidores (seguidor_id, seguido_id) VALUES ($seguidor_id, $id_usuario)";
-            if (!mysqli_query($conexion, $query)) {
-                exit();
-            }
+            mysqli_query($conexion, $query);
         }
+    }
 
-        header("Location: tablaSeguidores.php");
-        exit();
-    } 
+    header("Location: $pagina_origen");
+    exit();
 } else {
     exit();
 }
--- Php/Usuarios/tablaSeguidores.php
+++ Php/Usuarios/tablaSeguidores.php
@@ -1,10 +1,10 @@
 <?php
-    session_start();
-    if (!isset($_SESSION['username'])) {
+session_start();
+if (!isset($_SESSION['username'])) {
     header("Location: Php/Sesiones/inicio_sesion.php");
     exit();
 }
-    $id = intval($_SESSION['id']);
+$id = intval($_SESSION['id']);
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -16,33 +16,44 @@
 <body>
 <?php include __DIR__ . '/../Templates/navBar.php'; ?>
 
-    <div class="tabla-seguidores">
-        <h2>Seguidores</h2>
-        <table>
-            <tbody>
-                    <?php
-                        include '../../BD/conexiones.php';
-                        $query = "SELECT foto_perfil, username, id FROM usuarios join seguidores on id=seguidor_id where $id = seguido_id";
-                        $result = mysqli_query($conexion, $query);
+<div class="tabla-seguidores">
+    <h2>Seguidores</h2>
+    <table>
+        <tbody>
+            <?php
+            include '../../BD/conexiones.php';
+            
+            $query = "SELECT u.foto_perfil, u.username, u.id 
+                      FROM usuarios u
+                      JOIN seguidores s ON u.id = s.seguidor_id
+                      WHERE s.seguido_id = $id";
+            $result = mysqli_query($conexion, $query);
 
-                        if ($result && mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<tr>";
-                                echo "<td><img src='" . htmlspecialchars($row['foto_perfil']) . "' width='50' alt='Foto de perfil'></td>";
-                                echo "<td>" . htmlspecialchars($row['username']) . "</td>";
-                                echo "<td>
-                                        <form method='post' action='procesarSeguidores.php'>
-                                            <input type='hidden' name='id_usuario' value='" . $row['id'] . "'>
-                                            <input type='hidden' name='accion' value='seguir'>
-                                            <button type='submit'>Seguir</button>
-                                        </form>
-                                    </td>";
-                                echo "</tr>";
-                            }
-                        }
-                    ?>
-                </tbody>
-        </table>
-    </div>
+            if ($result && mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $usuario_id = intval($row['id']);
+                    $check = "SELECT 1 FROM seguidores WHERE seguidor_id = $id AND seguido_id = $usuario_id";
+                    $res_check = mysqli_query($conexion, $check);
+                    $accion = (mysqli_num_rows($res_check) > 0) ? 'suprimir' : 'seguir';
+                    $texto_boton = ($accion === 'suprimir') ? 'Suprimir' : 'Seguir';
+
+                    echo "<tr>";
+                    echo "<td><img src='" . htmlspecialchars($row['foto_perfil']) . "' width='50' alt='Foto de perfil'></td>";
+                    echo "<td>" . htmlspecialchars($row['username']) . "</td>";
+                    echo "<td>
+                            <form method='post' action='procesarSeguidores.php'>
+                                <input type='hidden' name='id_usuario' value='$usuario_id'>
+                                <input type='hidden' name='accion' value='$accion'>
+                                <input type='hidden' name='pagina_origen' value='" . basename($_SERVER['PHP_SELF']) . "'>
+                                <button type='submit'>$texto_boton</button>
+                            </form>
+                          </td>";
+                    echo "</tr>";
+                }
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
